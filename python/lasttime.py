bd1ec3204f929ab04d21d9d7d8f95c7de533e5d1
rover0811 <rover0811@hotmail.com>
2022-11-21 14:03:41 +0900

    [feat] 파이썬 df로 파싱 간단화

--- python/lasttime.py
+++ python/lasttime.py
@@ -6,7 +6,6 @@ import time
 import pandas as pd
 import re
 import datetime as dt
-
 ser = Service("C:/Users/rover0811/chromedriver.exe")  # 크롬 드라이버 잡아주는 것
 op = webdriver.ChromeOptions()  # initial
 op.add_experimental_option("excludeSwitches", ["enable-logging"])  # option 주기
@@ -21,32 +20,32 @@ ID = s.find_element(By.ID, "userid")
 PWD = s.find_element(By.ID, "pwd")
 LoginButton = s.find_element(By.CLASS_NAME, 'btn_login')
 
-ID.send_keys("")  # 학번
-PWD.send_keys('')  # 비밀번호
+ID.send_keys("비밀입니당")  # 학번
+PWD.send_keys('비밀이에염')  # 비밀번호
 LoginButton.click()
 
-time.sleep(5)
+time.sleep(3)
 
 학사관리Button = s.find_element(
     By.CSS_SELECTOR, "#ddba4fb5fbc996006194d3c0c0aea5c4 > a")
 
 학사관리Button.click()
 
-time.sleep(5)
+time.sleep(3)
 
 성적졸업Button = s.find_element(
     By.CSS_SELECTOR, r"#\38 d3da4feb86b681d72f267880ae8cef5 > a")
 
 성적졸업Button.click()
 
-time.sleep(5)
+time.sleep(3)
 
 s.switch_to.frame("URLSPW-0")
 time.sleep(2)
 
 s.find_element(By.CSS_SELECTOR, "#WD01C5").click()  # 여기서 에러가 나네?? 계절학기 이슈
 
-time.sleep(5)
+time.sleep(3)
 s.switch_to.parent_frame()
 
 s.switch_to.frame("contentAreaFrame")
@@ -58,62 +57,27 @@ time.sleep(3)
 html = s.page_source
 
 soup = BeautifulSoup(html, 'html.parser')
-tableitems = soup.select(
-    '#WD0186-contentTBody')
 
+tableitems = soup.find('tbody', {'id': "WD0186-contentTBody"})
+print(type(tableitems))
+print(tableitems)
 
-tableitems = tableitems[0].find_all('tr')
+soup = tableitems.find_all(
+    'span', {"style": "white-space:normal;"})
 
-data = {
-    "index": [""],
-    '이수학년도': [""],
-    '이수학기': [""],
-    '과목코드': [""],
-    '과목명': [""],
-    '과목학점': [""],
-    '과목성적': [""],
-    '과목등급': [""],
-    '교수명': [""],
-    '비고': [""],
-}
-# df = pd.DataFrame(data)
-data = []
-for i in tableitems:
-    newitem = list()
-    for k in i:
-        newitem.append(str(k.text[:(len(k.text)//2)]))
-    data.append(newitem)
-    # df.loc[len(df)] = newitem
-# for t in data:
-#     print(t)
+scorelist = []
+for index, value in enumerate(soup):
+    if (index % 8 == 0):
+        if (index == 0):
+            low = list()
+        else:
+            scorelist.append(low)
+            low = list()
+    else:
+        low.append(value.text)
+df = pd.DataFrame(scorelist, columns=[
+                  "이수학기", "과목코드", "과목명", "과목학점", "과목성적", "과목등급", "교수명"])
 
-df = pd.DataFrame(data, columns=data[0])
-df.to_excel("../result/result.xlsx")
-v = open(
-    "../result/seleniumpath.txt", "w+")
-# v = open("./variables.txt", "w")
-# f.write(str(tableitems))
-v.write("ID: {0}\n".format(ID))
-v.write("ID: {0}\n".format(PWD))
-v.write("ID: {0}\n".format(LoginButton))
-v.write("ID: {0}\n".format(학사관리Button))
-v.write("ID: {0}\n".format(성적졸업Button))
+df.to_excel("../result/practice.xlsx")
 
-v.close()
-
-# v.write(str(PWD))
-# v.write(str(LoginButton))
-# v.write(str(학사관리Button))
-# v.write(str(성적졸업Button))
-
-
-# actions = webdriver.ActionChains(s).move_to_element(
-#     닫기Button).click()
-
-# _cookies = s.get_cookies()
-# cookie_dict = {}
-# for cookie in _cookies:
-#     cookie_dict[cookie['name']] = cookie['value']
-# print(cookie_dict)
-
-# s.quit()
+# tableitems = tableitems[0].find_all('span', {'style': 'white-space: normal'})
